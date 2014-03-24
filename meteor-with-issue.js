0208233c046a8971884cb165d0591a4f0294c19a
Fabian Vogelsteller <fabian.vogelsteller@tunedin.de>
2014-03-24 14:08:17 +0100

    init

---
+++ meteor-with-issue.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+
+  Session.setDefault('rerunHelper', true);
+
+
+  Template.hello.myHelper = function () {
+    console.log('re-run #with helper');
+    return (Session.get('rerunHelper')) ? true : false;
+  };
+
+  Template.hello.events({
+    'click input.false': function () {
+      Session.set('rerunHelper', false);
+    },
+    'click input.rerun': function () {
+      Session.set('rerunHelper', Math.random(0,99));
+    }
+  });
+}
